5d5a6237219d4787cbb2b8daf8e1b98ab52c2756
廖子斌 <liaozb@wangsu.com>
2019-07-26 16:17:50 +0800

    第一章：基础类型课程完善

--- src/views/BaseType.vue
+++ src/views/BaseType.vue
@@ -36,23 +36,47 @@
       <p>list([1, 2, 3])
         <span>{{list}}</span>
       </p>
+      <p>list([1, '2', 3])
+        <span>{{list[1]}}</span>
+      </p>
       <p>
         listGeneric({{'Array<类型>'}}[1, 2, 3])
         <span>{{listGeneric}}</span>
       </p>
+    </div>
+    <div>
+      <h2>元组Tuple</h2>
       <p>x([string, number]['hello', 10])
         <span>{{x}}</span>
       </p>
       <button @click='tupleError'>元组错误</button>
     </div>
     <div>
-      <h2>枚举</h2>c(Color.Green)
+      <h2>枚举（枚举从0开始编号，支持自定义编号）</h2>c(Color.Green)
       <span>{{c}}</span>
+      <p>colorName(Color[2])
+        <span>{{colorName}}</span>
+      </p>
+      
     </div>
     <div>
       <h2>Any</h2>notSure(any)
       <span>{{notSure}}</span>
       <button @click='changeAny'>修改any</button>
+    </div>
+    <div>
+      <h2>Void</h2>
+      <button @click='warnUser'>warnUser</button>
+    </div>
+    <div>
+      <h2>null和undefined</h2>
+    </div>
+    <div>
+      <h2>never不能结束，常用于抛出错误</h2>
+      <button @click='fail'>fail</button>
+    </div>
+    <div>
+      <h2>object(通常与接口联用)</h2>
     </div>
     <div>
       <h2>类型断言</h2>
@@ -74,37 +98,49 @@
 
 <script lang='ts'>
 import { Component, Vue } from 'vue-property-decorator'
+// 枚举从0开始编号，支持自定义编号
 enum Color {
   Red,
   Green,
   Blue
 }
 
+// 创建objcet
+declare function create(o: object | null): void
+create({prop: 0})
+create(null)
+
 @Component({})
 export default class BaseType extends Vue {
+  // 布尔
   public isDone: boolean = false
-  public decLiteral: number = 6
-  public hexLiteral: number = 0xf00d
-  public binaryLiteral: number = 0b1010
-  public octalLiteral: number = 0o744
+  // 数字
+  public decLiteral: number = 20
+  public hexLiteral: number = 0x14 // 16进制
+  public binaryLiteral: number = 0b10100 // 2进制
+  public octalLiteral: number = 0o24 // 8进制
+  // 字符串
   public name: string = 'bob'
   public sentence: string = `Hello, my name is ${this.name}.`
-  public list: number[] = [1, 2, 3]
+  // 数组
+  public list: (number | string)[] = [1, '2s', 3]
   public listGeneric: Array<number> = [1, 2, 3]
+  // 元组
   public x: [string, number] = ['hello', 10]
+  // 枚举
   public c: Color = Color.Green
+  public colorName: string = Color[2]
+  // any
   public notSure: any = 4
+  // void、null、undefined、never
+  public unusable: void = undefined // 无意义
+  public num: number = 3
+  // 类型判定
   public someValue: any = 'this is a string'
   public strLength: number = (<string>this.someValue).length
   public someValueAs: any = 'this is a string(as)'
   public strLengthAs: number = (this.someValueAs as string).length
-
-  changeAny() {
-    this.notSure = 'maybe a string instead'
-    setTimeout(() => {
-      this.notSure = false // okay, definitely a boolean
-    }, 2000)
-  }
+  
   tupleError() {
     this.x = [10, 'hello']
     console.log(this.x[0].substr(1)) // OK
@@ -112,6 +148,29 @@ export default class BaseType extends Vue {
     this.x[3] = 'world' // OK, 字符串可以赋值给(string | number)类型
     console.log(this.x[5].toString()) // OK, 'string' 和 'number' 都有 toString
     this.x[6] = true // Error, 布尔不是(string | number)类型
+  }
+  changeAny() {
+    this.notSure = 'maybe a string instead'
+    setTimeout(() => {
+      this.notSure = false // okay, definitely a boolean
+    }, 2000)
+  }
+  warnUser(): void {
+    console.log('This is my warning message.')
+  }
+  numToNull() {
+    this.num = null // 当指定了strictNullChecks标记，null和undefined只能赋值给void和它们各自
+  }
+  error(message: string): never { // 不能结束，常用于抛出错误
+    throw new Error()
+  }
+  fail() {
+    return this.error('Something failed')
+  }
+  inifiniteLoop(): never {
+    while (true) {
+      // 无限循环，也符合never的规则
+    }
   }
 }
 </script>
